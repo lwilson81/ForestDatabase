c72d615a71ca68e984b4b5b65ca7b131c0029d2c
mshen63 <mshen63@gatech.edu>
2022-02-10 22:03:51 -0500

    Fix create table queries and implemented fill_db and insert_step functions

--- create_database_for_dances.py
+++ create_database_for_dances.py
@@ -1,118 +1,117 @@
 import pymysql
 import getpass
 from steps import *
+import json
+# import pprint
 
 #   - use the connection in the parameters to create a cursor
 #   - use the cursor, to execute SQL statements
 #   - call connection.commit() to save changes you make to the database
 #
 
+
 def reset_database(connection):
     """
     this method creates tables. if the tables already exist, drop them first.
-
     returns: None
     """
-   
-    # , query1, query2, statement]
-    dropTables = 'DROP TABLE IF EXISTS dances, steps;' #insert table names here
+
+    dropTables = 'DROP TABLE IF EXISTS dances, steps;'  # insert table names here
 
     createDanceTable = """CREATE TABLE dances (
         name VARCHAR(255) NOT NULL,
-        danceId bigint(20) NOT NULL AUTO_INCREMENT,
-        moves JSON,
-        PRIMARY KEY ('danceId)
+        moves varchar(255),
+        PRIMARY KEY (name)
+    ); """
 
-    ); """ 
-    
-    # joint1 JSON will be [angle, time]   
+    # joint1 JSON will be JSON {}
     createStepTable = """CREATE TABLE steps (
         name VARCHAR(255), 
-        stepId bigint(20) NOT NULL AUTO_INCREMENT,
-        joint1 JSON, 
-        joint2 JSON, 
-        joint3 JSON, 
-        joint4 JSON, 
-        joint5 JSON, 
-        joint6 JSON, 
-        joint7 JSON, 
+        joint1 varchar(255), 
+        joint2 VARCHAR(255), 
+        joint3 VARCHAR(255), 
+        joint4 VARCHAR(255), 
+        joint5 VARCHAR(255), 
+        joint6 VARCHAR(255), 
+        joint7 VARCHAR(255), 
+        PRIMARY KEY (name)
     );"""
 
-    creationQueries = [dropTables, createDanceTable, createStepTable]
-    for query in creationQueries:
+    cursor = connection.cursor()
+    for query in [dropTables, createDanceTable, createStepTable]:
         cursor.execute(query)
 
-    statement = "INSERT INTO geekstudent( id, name,gender, subject)\
-VALUES(6,'Shoit','M', 'ML')"
-    cursor = connection.cursor()
-    
-   
     cursor.close()
     connection.commit()
-    
-    return 
+
 
 def fill_db():
+    cursor = connection.cursor()
+
+    def namestr(obj, namespace):
+        return [name for name in namespace if namespace[name] is obj]
+
     for step in dance_step_database:
+        step_name = namestr(step, globals())[0]
         angles = step[0]
         times = step[1]
-        # check if element is an array, if not make it one and then insert into DB
-        # for i in len(angles):
-            
-        # for ___ in angles:
-        #     if !__ isinstance([0, 10, 20, 30], list):
-        #         __ = [__]
+        json_list = list()
+
+        for i in range(len(angles)):
+            joint_dict = dict()
+            joint_dict["angles"] = angles[i]
+            joint_dict["times"] = times[i]
+            json_list.append(json.dumps(joint_dict))
+        addStep = """INSERT INTO `steps` (name, joint1, joint2, joint3, joint4, joint5, joint6, joint7) VALUES(%s, %s, %s, %s, %s, %s, %s, %s)"""
+        cursor.execute(addStep, (step_name, *json_list))
+
+    cursor.close()
+    connection.commit()
 
 
-
-
-def insert_step(name, angle1, angle2, angle3, angle4, angle5, angle6, angle7): #insert attributes
+def insert_step(step_name, *joints):  # insert attributes
     """
     insert a new row into the table using the parameters given. you don't
     need to input the petID column, but you MUST use %s for the other columns.
 
     returns: None
     """
-    query = 'INSERT INTO steps VALUES (%s, %s, %s, %s, %s, %s, %s, %s)'
     cursor = connection.cursor()
-    cursor.execute(query, (name, angle1, angle2, angle3, angle4, angle5, angle6, angle7))
+    addStep = """INSERT INTO `steps` (name, joint1, joint2, joint3, joint4, joint5, joint6, joint7) VALUES(%s, %s, %s, %s, %s, %s, %s, %s)"""
+    cursor.execute(addStep, (step_name, *joints))
     cursor.close()
     connection.commit()
 
-#main method to execute
+# main method to execute
+
+
 def main():
-    ##change database to name of database
-    
+    # change database to name of database
+
     database = 'Forest'
-    user_password = getpass.getpass(prompt = 'Enter Password: ')
+    user_password = getpass.getpass(prompt='Enter Password: ')
     global connection
     connection = pymysql.connect(host='localhost',
                                  user='root',
                                  password=user_password,
-                                 db= None,
+                                 db=None,
                                  charset="utf8mb4",
                                  cursorclass=pymysql.cursors.Cursor)
 
-    cursor = connection.cursor()
 
+    ############ TO CREATE DATABASE UNCOMMENT THIS SECTION ############
     # cursor.execute("CREATE DATABASE Forest;")
-    cursor.execute("USE Forest;")
+    # cursor.execute("USE Forest;")
+    ###################################################################
 
-
-    ###creates tables
-    #*****UNCOMMENT TO RESET DATABASE********8
-    reset_database(connection)
-
-    # insert_step(connection, "ISO_LEFT", 0, 0, 20, 0, 45, 0, 0)
-
-    ###
-
-    # print(cursor.fetchall())
-
-    ###
-    
+    ####### TO RESET AND REFILL DATABASE UNCOMMENT THIS SECTION #######
+    # cursor.execute("USE Forest;")
+    # reset_database(connection)
+    # fill_db()
+    ###################################################################
 
     connection.close()
+
 
 if __name__ == "__main__":
     main()
